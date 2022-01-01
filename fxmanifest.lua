1101ea78a8f11bce9db1fa06843d62be6dd26162
Nu Imran <39611124+imran123bd@users.noreply.github.com>
2022-01-02 01:42:27 +0600

    v1

---
+++ fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Redven Biker (XLife.fr)'
+description 'Discord : https://discord.gg/C3aN2bBVbB'
+version '1.0.0'
+
+loadscreen 'index.html'
+loadscreen_manual_shutdown 'yes'
+client_script 'client.lua'
+
+files {
+    'index.html',
+    'style.css',
+    'script.js',
+    'img/logo.png',
+    'img/background1.jpg',
+    'img/background2.jpg',
+    'img/background3.jpg',
+    'img/background4.jpg',
+    'img/icons/discord.png',
+    'img/icons/gamemode.png',
+    'img/icons/user.png',
+    'img/icons/www.png',
+    'music/music.mp3'
+}
